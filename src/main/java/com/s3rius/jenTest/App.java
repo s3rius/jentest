c8fb24201ddfc5e62c0182b6e9f25e4d2a7cab70
Pavel Kirilin <win10@list.ru>
2022-05-07 03:25:32 +0400

    Initial commit.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

---
+++ src/main/java/com/s3rius/jenTest/App.java
@@ -0,0 +1,13 @@
+package com.s3rius.jenTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
